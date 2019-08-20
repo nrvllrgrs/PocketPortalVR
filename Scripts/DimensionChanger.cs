e80d238ecfaabf46839b3870efceb25ecec606a1
Chad Kilgore <ckilgore@gmail.com>
2019-08-19 23:44:07 -0500

    Added ability to register dimension changed event

--- Scripts/DimensionChanger.cs
+++ Scripts/DimensionChanger.cs
@@ -6,29 +6,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class DimensionChanger {
-	public static void SwitchDimensions(GameObject obj, Dimension fromDimension, Dimension toDimension) {
+public static class DimensionChanger
+{
+	#region Events
+
+	public static event DimensionEventHandler Changed;
+
+	#endregion
+
+	#region Methods
+
+	public static void SwitchDimensions(GameObject obj, Dimension fromDimension, Dimension toDimension)
+	{
 		obj.layer = toDimension.layer;
 
         // Move over all visible children as well
-        foreach(MeshRenderer childRenderer in obj.GetComponentsInChildren<MeshRenderer>()) {
+        foreach(MeshRenderer childRenderer in obj.GetComponentsInChildren<MeshRenderer>())
+		{
             childRenderer.gameObject.layer = toDimension.layer;
         }
 
 		// If this is an FPS controller then make sure it goes through too.
 		Transform parent = obj.transform.parent;
-		if(parent != null && parent.GetComponent<CharacterController>()) {
+		if(parent != null && parent.GetComponent<CharacterController>())
+		{
 			parent.gameObject.layer = toDimension.layer;
 		}
+
+		// Notify listeners
+		Changed?.Invoke(obj, new DimensionEventArgs(fromDimension, toDimension));
 	}
 
-	public static void SwitchCameraRender(Camera camera, int fromDimensionLayer, int toDimensionLayer, Material dimensionSkybox) {
-		CameraExtensions.LayerCullingShow (camera, toDimensionLayer);
-		CameraExtensions.LayerCullingHide (camera, fromDimensionLayer);
-		if (dimensionSkybox) {
-			if (camera.GetComponent<Skybox> ()) {
-				camera.GetComponent<Skybox> ().material = dimensionSkybox;	
+	public static void SwitchCameraRender(Camera camera, int fromDimensionLayer, int toDimensionLayer, Material dimensionSkybox)
+	{
+		CameraExtensions.LayerCullingShow(camera, toDimensionLayer);
+		CameraExtensions.LayerCullingHide(camera, fromDimensionLayer);
+
+		if (dimensionSkybox)
+		{
+			var skybox = camera.GetComponent<Skybox>();
+			if (skybox != null)
+			{
+				skybox.material = dimensionSkybox;	
 			}
 		}
 	}
+
+	#endregion
+}
+
+public delegate void DimensionEventHandler(object sender, DimensionEventArgs e);
+
+public class DimensionEventArgs : System.EventArgs
+{
+	#region Properties
+
+	public Dimension fromDimension { get; private set; }
+	public Dimension toDimension { get; private set; }
+
+	#endregion
+
+	#region Constructors
+
+	public DimensionEventArgs(Dimension fromDimension, Dimension toDimension)
+	{
+		this.fromDimension = fromDimension;
+		this.toDimension = toDimension;
+	}
+
+	#endregion
 }
